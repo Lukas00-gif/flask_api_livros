10d6801069bad4e8e0dd5524c071d2bda5e906b6
Lukas00-gif <lucassky08@gmail.com>
2023-08-02 17:30:52 -0300

    adds excluir criar editarid

--- app.py
+++ app.py
@@ -27,6 +27,40 @@ livros = [
 def obter_livros():
     return jsonify(livros)
 
+# consultar por id GET
+# passar por todos os id e ve se e o que eu quero
+@app.route('/livros/<int:id>', methods=['GET'])
+def obter_livros_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+
+
+#editar id
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+
+#criar
+@app.route('/livros', methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    return jsonify(livros)
+
+#excluir
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir_livros(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+    
+    return jsonify(livros)
 
 
 app.run(port=7000, host='localhost', debug=True)
